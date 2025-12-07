3fd0b9fee2fd1ded97f5cfb5cd702dca34e45965
abrilberliando <abrilbrlnd2@gmail.com>
2025-12-08 06:17:36 +0700

    Fix: Automatic Hero

--- app/Models/HeroSection.php
+++ app/Models/HeroSection.php
@@ -10,11 +10,16 @@ class HeroSection extends Model
     use HasFactory;
 
     protected $fillable = [
-        'background_image',
+        'background_images', // 👈 Ganti/Tambah field ini
         'subtitle',
         'title',
         'description',
         'button_text',
         'button_link',
+    ];
+
+    // 👇 Tambahin casting buat 'background_images' biar otomatis jadi array
+    protected $casts = [
+        'background_images' => 'array',
     ];
 }
