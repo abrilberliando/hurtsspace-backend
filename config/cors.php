226911f86f5ee19692e484484fcec5333d454c8d
abrilberliando <abrilbrlnd2@gmail.com>
2025-12-11 04:39:51 +0700

    cors update

--- config/cors.php
+++ config/cors.php
@@ -9,7 +9,7 @@ return [
     'allowed_origins' => [
         'https://hurtsspace.com',
         'http://localhost:3000',
-        'https://hurtsspace-frontend-abril-berliandos-projects.vercel.app/',
+        'https://hurtsspace-frontend-abril-berliandos-projects.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
