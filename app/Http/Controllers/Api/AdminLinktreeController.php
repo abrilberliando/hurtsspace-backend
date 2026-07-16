8c345942316ed58de535546797721c3d789b6ac5
abrilberliando <abrilbrlnd2@gmail.com>
2026-07-16 17:47:51 +0700

    feat: add DB facade import to AdminLinktreeController

--- app/Http/Controllers/Api/AdminLinktreeController.php
+++ app/Http/Controllers/Api/AdminLinktreeController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class AdminLinktreeController extends Controller
 {
